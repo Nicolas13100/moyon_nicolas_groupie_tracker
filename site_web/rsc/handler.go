853f4bfd6a92d6d8c78c329c53bc26fcfd9a377d
Nicolas <nicolas.moyon@ynov.com>
2024-01-29 14:23:55 +0100

    seneding to main pc

--- site_web/rsc/handler.go
+++ site_web/rsc/handler.go
@@ -8,13 +8,14 @@ import (
 
 func RUN() {
 	// used same system than hangman, since it was working prety well
-	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", ErrorHandler)
+	http.HandleFunc("/home", indexHandler)
 
 	// Serve static files from the "site_web/static" directory << modified from hangman
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../site_web/static"))))
 
 	// Print statement indicating server is running << same
-	fmt.Println("Server is running on :8080 http://localhost:8080")
+	fmt.Println("Server is running on :8080 http://localhost:8080/home")
 
 	// Start the server << same
 	if err := http.ListenAndServe(":8080", nil); err != nil {
